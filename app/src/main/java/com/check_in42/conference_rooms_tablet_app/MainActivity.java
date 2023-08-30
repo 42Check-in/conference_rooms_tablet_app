e94907239b436cffb19454cd9d54d53f0db1ed8d
이현수 <dldlsrn231@gmail.com>
2023-08-30 19:24:50 +0900

    update

---
+++ app/src/main/java/com/check_in42/conference_rooms_tablet_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.check_in42.conference_rooms_tablet_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "conference_rooms_tablet_app"
+include(":app")
